84853486e85959058e3f829be4295f655d50c2c5
Yaşar BAYKUŞ <byyasar@gmail.com>
2021-02-07 02:38:35 +0300

    başla durdur eklendi.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,15 +16,16 @@ class _HomePageState extends State<HomePage> {
   int sayac = 0;
   Offset eskiOffset = Offset(0, 0);
   Offset yeniOffset = Offset(0, 0);
+  CustomPaint yeniPaint = CustomPaint();
+  CustomPaint eskiPaint = CustomPaint();
+  Timer timer;
+  bool durum = false;
 
   @override
   void initState() {
     // TODO: implement initState
-    
     list = ledleriOlustur();
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {calistir();});
-    });
+
     super.initState();
   }
 
@@ -34,10 +35,11 @@ class _HomePageState extends State<HomePage> {
     final double _height = MediaQuery.of(context).size.height.toDouble();
 
     return Scaffold(
-        appBar: AppBar(),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            calistir();}),
+            backgroundColor: Colors.red,
+            onPressed: () {
+              BaslaDurdur();
+            }),
         body: Container(
             width: _width,
             height: _height,
@@ -47,49 +49,62 @@ class _HomePageState extends State<HomePage> {
                 angle: -pi / 2, child: Stack(children: list))));
   }
 
-  Function calistir(){
-    if (sayac > 0) {
-              eskiOffset = listOffset[sayac - 1];
-              yeniOffset = listOffset[sayac];
-              CustomPaint yeniPaint = CustomPaint(
-                  painter: CustomLedWidget(
-                      yeniOffset.dx, yeniOffset.dy, 15, Colors.yellow));
-              CustomPaint eskiPaint = CustomPaint(
-                  painter: CustomLedWidget(
-                      eskiOffset.dx, eskiOffset.dy, 15, Colors.pink));
-              list[sayac - 1] = eskiPaint;
-              list[sayac] = yeniPaint;
-              if (sayac == list.length - 1) {
-                eskiOffset = listOffset[sayac];
-                sayac = 0;
-              } else {
-                sayac++;
-              }
-            } else {
-              CustomPaint eskiPaint = CustomPaint(
-                  painter: CustomLedWidget(
-                      eskiOffset.dx, eskiOffset.dy, 15, Colors.pink));
-              if (eskiOffset.dx != 0.0) {
-                print('sona geldi');
-                list[list.length - 1] = eskiPaint;
-              }
-              Offset yeniOffset = listOffset[sayac];
-              CustomPaint yeniPaint = CustomPaint(
-                  painter: CustomLedWidget(
-                      yeniOffset.dx, yeniOffset.dy, 15, Colors.yellow));
-              list[sayac] = yeniPaint;
-              sayac++;
-            }
+  void BaslaDurdur() {
+    print(durum);
+    durum = !durum;
+    if (durum) {
+      timer = new Timer.periodic(Duration(milliseconds: 75), (timers) {
+        setState(() {
+          print('tick çalışıyor');
+          calistir();
+          if (durum == false) timer.cancel();
+        });
+      });
+    }
+  }
 
-            setState(() {
-              print(sayac);
-            });
-          }
+  void calistir() {
+    if (sayac > 0) {
+      eskiOffset = listOffset[sayac - 1];
+      yeniOffset = listOffset[sayac];
+      yeniPaint = CustomPaint(
+          painter:
+              CustomLedWidget(yeniOffset.dx, yeniOffset.dy, 15, Colors.yellow));
+      eskiPaint = CustomPaint(
+          painter:
+              CustomLedWidget(eskiOffset.dx, eskiOffset.dy, 15, Colors.pink));
+      list[sayac - 1] = eskiPaint;
+      list[sayac] = yeniPaint;
+      if (sayac == list.length - 1) {
+        eskiOffset = listOffset[sayac];
+        sayac = 0;
+      } else {
+        sayac++;
+      }
+    } else {
+      eskiPaint = CustomPaint(
+          painter:
+              CustomLedWidget(eskiOffset.dx, eskiOffset.dy, 15, Colors.pink));
+      if (eskiOffset.dx != 0.0) {
+        print('sona geldi');
+        list[list.length - 1] = eskiPaint;
+      }
+      yeniOffset = listOffset[sayac];
+      yeniPaint = CustomPaint(
+          painter:
+              CustomLedWidget(yeniOffset.dx, yeniOffset.dy, 15, Colors.yellow));
+      list[sayac] = yeniPaint;
+      sayac++;
+    }
+
+    setState(() {
+      print(sayac);
+    });
   }
 
   List<Widget> ledleriOlustur() {
     var innerCircleRadius = 400;
-    for (double i = 0; i < 360; i += 30) {
+    for (double i = 0; i < 360; i += 12) {
       var x = innerCircleRadius * cos(i * pi / 180);
       var y = innerCircleRadius * sin(i * pi / 180);
       list.add(CustomPaint(painter: CustomLedWidget(x, y, 15, Colors.pink)));
